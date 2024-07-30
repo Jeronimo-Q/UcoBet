ec2575e37d39b47d5d364ac590395d2f6f7e9d3c
Jeronimo Quintero <jeronimoq18@gmail.com>
2024-07-30 11:04:38 -0500

    Creación menu

--- src/com/jeronimo/ucobet/app/AppUcoBet.java
+++ src/com/jeronimo/ucobet/app/AppUcoBet.java
@@ -1,16 +1,14 @@
 package com.jeronimo.ucobet.app;
+import com.jeronimo.ucobet.dominio.Menu;
 import com.jeronimo.ucobet.dominio.UcoBet;
 
 public class AppUcoBet {
 
     public static void main(String[] args) {
 
-        UcoBet apuestas = new UcoBet();
+        Menu menu = new Menu();
 
         //Ingreso de datos desde la consola del usuario
-        for (int i = 0; i < 2; i++) {
-           apuestas.ingreso();
-           apuestas.historial();
-        }
+        menu.mostrarMenu();
     }
 }
